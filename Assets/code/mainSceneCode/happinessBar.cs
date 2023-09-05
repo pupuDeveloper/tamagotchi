3927009e318fec2a95fef6329f0f17f8a3dfe784
SaimiVilkkilä <90615101+pupuDevelo@users.noreply.github.comper>
2023-09-05 11:52:49 +0300

    happiness bar
    Added a happiness bar updates negatively when poops spawn, and positively when player cleans them

---
+++ Assets/code/mainSceneCode/happinessBar.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class happinessBar : MonoBehaviour
+{
+
+    [SerializeField] private Image happinessbarFill;
+
+
+//this function adds or subtracts happiness points from the bar. 0 is 0 and 1 is full (0.5 is half bar)
+    public void UpdateHappinessBar()
+    {
+        happinessbarFill.fillAmount = GameManager.Instance.happiness;
+    }
+}
