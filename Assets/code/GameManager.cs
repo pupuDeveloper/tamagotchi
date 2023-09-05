848aa8f7da33c402deed7257620b1e2eb59a2c25
SaimiVilkkilä <90615101+pupuDevelo@users.noreply.github.comper>
2023-09-05 10:03:15 +0300

    all shit fixed lets go

---
+++ Assets/code/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("Gamemanager is NULL");
+            }
+
+            return _instance;
+        }
+    }
+    public int poopAmount {get; set;}
+    public int happiness {get; set;}
+
+    private void Awake()
+    {
+        if (_instance)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+        DontDestroyOnLoad(this);
+    }
+}
