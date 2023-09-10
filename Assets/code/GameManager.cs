e71437b754490969c7cb47b9553fc3b7a2337790
saimivilkkila <saimi.vilkkila@gmail.com>
2023-09-10 16:02:13 +0300

    sadad

--- Assets/code/GameManager.cs
+++ Assets/code/GameManager.cs
@@ -1,47 +1,48 @@
-using System.Collections; 
-using System.Collections.Generic; 
-using UnityEngine; 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class GameManager : MonoBehaviour 
-{ 
-    private static GameManager _instance; 
-    public static GameManager Instance 
-    { 
-        get 
-        { 
-            if (_instance == null) 
-            { 
-                Debug.LogError("Gamemanager is NULL"); 
-            } 
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("Gamemanager is NULL");
+            }
 
-            return _instance; 
-        } 
-    } 
-    public int poopAmount {get; set;} 
-    public float happiness {get; set;} 
+            return _instance;
+        }
+    }
+    public int poopAmount { get; set; }
+    public float happiness { get; set; }
+    public int day { get; set; }
 
-    private void Awake() 
-    { 
-        happiness = 0.5f; 
-        if (_instance) 
-        { 
-            Destroy(gameObject); 
-        } 
-        else 
-        { 
-            _instance = this; 
-        } 
-        DontDestroyOnLoad(this); 
-    } 
-    void Update() 
-    { 
-        if (happiness > 1) 
-        { 
-            happiness = 1; 
-        } 
-        else if (happiness < 0) 
-        { 
-            happiness = 0; 
-        } 
-    } 
-} 
+    private void Awake()
+    {
+        happiness = 0.5f;
+        if (_instance)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+        DontDestroyOnLoad(this);
+    }
+    void Update()
+    {
+        if (happiness > 1)
+        {
+            happiness = 1;
+        }
+        else if (happiness < 0)
+        {
+            happiness = 0;
+        }
+    }
+}
