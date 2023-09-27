8754f5b0b2ad36526ac6a8ecd825aad979b2bc00
SaimiVilkkilä <90615101+pupuDevelo@users.noreply.github.comper>
2023-09-27 13:53:16 +0300

    added saturday bunny art and last day bg for 1st build

--- Assets/code/mainSceneCode/moveBackground.cs
+++ Assets/code/mainSceneCode/moveBackground.cs
@@ -19,11 +19,11 @@ public class moveBackground : MonoBehaviour
     {
         if (hovering)
         {
-            if (this.gameObject.name == "right" && background.transform.position.x < 10)
+            if (this.gameObject.name == "right" && background.transform.position.x < 5)
             {
                 background.transform.position += new Vector3(7 * Time.deltaTime, 0, 0);
             }
-            else if (this.gameObject.name == "left" && background.transform.position.x > -10)
+            else if (this.gameObject.name == "left" && background.transform.position.x > -5)
             {
                 background.transform.position -= new Vector3(7 * Time.deltaTime, 0, 0);
             }
