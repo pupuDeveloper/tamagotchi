8e35ba75121dcefd927cd562d3d88f16f4a2a825
SaimiVilkkilä <90615101+pupuDevelo@users.noreply.github.comper>
2023-10-03 00:48:44 +0300

    brushing progress

--- Assets/code/mainSceneCode/petButton.cs
+++ Assets/code/mainSceneCode/petButton.cs
@@ -6,20 +6,35 @@ using UnityEngine.UI;
 public class petButton : MonoBehaviour
 {
     private bool isButtonAvailable;
+    private bool isBrushingOn;
     private happinessBar happinessbar;
     public GameObject happinessBarScriptHolder;
     public Button petbutton;
     void Awake()
     {
         happinessbar = happinessBarScriptHolder.GetComponent<happinessBar>();
+        isBrushingOn = false;
     }
 
     public void petbuttonFunc()
     {
+        if (isBrushingOn)
+        {
+            isBrushingOn = false;
+        }
+        else
+        {
+            isBrushingOn = true;
+        }
+
+
+
+        /*
         GameManager.Instance.happiness += 0.075f;
         happinessbar.UpdateHappinessBar();
         petbutton.interactable = false;
         GameManager.Instance.brushPet = true;
+        */
     }
 
     void FixedUpdate()
