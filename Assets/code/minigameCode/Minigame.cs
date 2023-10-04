e64b42fb0d07502ce9a5a9df893d214b62db7029
Sonja <sonja.keinanen@tuni.fi>
2023-10-04 11:43:05 +0300

    strawberries spawning

--- Assets/code/minigameCode/Minigame.cs
+++ Assets/code/minigameCode/Minigame.cs
@@ -5,17 +5,15 @@ using UnityEngine;
 
 public class Minigame : MonoBehaviour
 {
-
-    private float _xPos;
-    private Vector3 _newPos;
+    [SerializeField] private GameObject _berryPrefab;
+    //private float _xPos;
+    //private Vector3 _newPos;
     private float _fallSpeed = 6.0f;
     private float _spinSpeed = 250.0f;
     private GameObject popoutwindow;
     private void Awake()
     {
         popoutwindow = GameObject.Find("infoPopout");
-        _xPos = Random.Range(-2.51f, 1.66f);
-        _newPos = new Vector3(_xPos, 6, 0);
     }
 
     private void FixedUpdate()
@@ -23,17 +21,11 @@ public class Minigame : MonoBehaviour
         if (popoutwindow.activeSelf == false)
         {
             //move the object down the screen
-        transform.Translate(Vector3.down * _fallSpeed * Time.deltaTime, Space.World);
-        transform.Rotate(Vector3.forward, _spinSpeed * Time.deltaTime);
+            transform.Translate(Vector3.down * _fallSpeed * Time.deltaTime, Space.World);
+            transform.Rotate(Vector3.forward, _spinSpeed * Time.deltaTime);
 
-        //if the object has reached the bottom of the screen
-        if(transform.position.y < -6)
-        {
-            //move the object above the screen
-            _xPos = Random.Range(-2.51f, 1.66f);
-            _newPos = new Vector3(_xPos, 6, 0);
-            transform.position = _newPos;
-        }
+            Vector3 pos = new Vector3(Random.Range(-2.51f, 1.66f), 6, Random.Range(-2.51f, 1.66f));
+            Instantiate(_berryPrefab, pos, Quaternion.identity);
         }
     }
 }
