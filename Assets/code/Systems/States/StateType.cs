f1ff5a6ba75a1fbc0482755ae74c83a6bef7403f
Sonja <sonja.keinanen@tuni.fi>
2023-10-25 14:08:04 +0300

    Scene Management started
    continuing later at better time to complete this

---
+++ Assets/code/Systems/States/StateType.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BunnyHole.States
+{
+    public enum StateType
+    {
+        None = 0,
+        MainMenu,
+        MainScene,
+        Options,
+        Credits,
+        Minigame,
+        Pause,
+        GameOver
+    }
+
+}
