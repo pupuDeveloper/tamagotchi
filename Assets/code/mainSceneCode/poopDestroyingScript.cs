b50ab687e3c47a62d328c75b38896dafa58bf70e
saimivilkkila <saimi.vilkkila@gmail.com>
2023-11-19 17:35:56 +0200

    Poop cleaning anim added

--- Assets/code/mainSceneCode/poopDestroyingScript.cs
+++ Assets/code/mainSceneCode/poopDestroyingScript.cs
@@ -7,20 +7,27 @@ public class poopDestroyingScript : MonoBehaviour
 
     private poopScript poopscript;
     private happinessBar happinessbar;
+    private Animator cleaningAnim;
+    private GameObject cleaninOb;
 
     void Awake()
     {
         poopscript = GameObject.Find("poopSpawner").GetComponent<poopScript>();
         happinessbar = GameObject.Find("happinesbarBackground").GetComponent<happinessBar>();
+        cleaninOb = GameObject.Find("poopSpawner");
+        cleaningAnim = cleaninOb.GetComponentInChildren<Animator>(true);
     }
     void OnMouseDown()
     {
         if (poopscript.isCleaningOn)
         {
+        Vector2 pos = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 0.15f);
+        cleaninOb.transform.position = pos;
         GameManager.Instance.poops.Remove(gameObject.transform.position);
         gameObject.GetComponent<SpriteRenderer>().enabled = false;
         GameManager.Instance.happiness += 0.03f;
         StartCoroutine(happinessbar.particle(3));
+        cleaningAnim.SetTrigger("clean");
         StartCoroutine("destroyDelay");
         }
     }
