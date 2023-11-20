71c8260579c25464301f0a682593f576f4f2770f
saimivilkkila <saimi.vilkkila@gmail.com>
2023-11-20 23:02:27 +0200

    some progres

--- Assets/code/dataSaving/BinarySaver.cs
+++ Assets/code/dataSaving/BinarySaver.cs
@@ -5,6 +5,46 @@ using System.IO;
     {
         private BinaryReader _reader;
         private BinaryWriter _writer;
+
+        //called before reading the data from the save file. Opens the filestream and creates the binary reader
+        public bool PrepareRead(string path)
+        {
+            //check if save file Exists
+            if (!File.Exists(path))
+            {
+                return false;
+            }
+
+            try
+            {
+                _fileStream = File.Open(path, FileMode.Open, FileAccess.Read);
+                _reader = new BinaryReader(_fileStream);
+            }
+            catch(Exception e)
+            {
+                Debug.LogException(e);
+                return false;
+            }
+            return true;
+        }
+        
+        // Called after all data is read from the file to release the save file and memory
+        public void FinalizeRead()
+        {
+            _reader.Close();
+            _fileStream.Close();
+
+            _reader = null;
+            _fileStream = null;
+        }
+
+        public bool PrepareWrite(string path)
+        {
+            try
+            {
+                string directory
+            }
+        }
         private FileStream _fileStream;
 
         #region Reading
