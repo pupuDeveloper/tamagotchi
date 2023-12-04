5bce77d4e6fe819bb04f8785b4ccc7c7afd35caa
Sonja <sonja.keinanen@tuni.fi>
2023-12-04 14:09:40 +0200

    Minigame testing
    stuff to do

--- Assets/code/minigameCode/OnCollision.cs
+++ Assets/code/minigameCode/OnCollision.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class Collision : MonoBehaviour
+namespace BunnyHole
 {
-    private Vector3 _newPos;
-    private void OnCollisionEnter2D(Collision2D collision)
+    public class Collision : MonoBehaviour
     {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Floor"))
+        /// <summary>
+        /// New position for the spawned berries and eyeballs.
+        /// </summary>
+        private Vector3 _newPos;
+
+        /// <summary>
+        /// If collision happened, we go here.
+        /// </summary>
+        /// <param name="collision"></param>
+        private void OnCollisionEnter2D(Collision2D collision)
         {
-            float _xPos = Random.Range(-3.85f, 3.80f);
-            _newPos = new Vector3(_xPos, 6, 0);
-            transform.position = _newPos;
+            // Is the collision happening with the floor? If yes..
+            if (collision.gameObject.layer == LayerMask.NameToLayer("Floor"))
+            {
+                // Setting the new random x-axis position between -3.85 and 3.80
+                float _xPos = Random.Range(-3.85f, 3.80f);
+                // Setting the new position for the berries and eyeballs:
+                // random range x-axis, y-axis is at 6 and z 0.
+                _newPos = new Vector3(_xPos, 6, 0);
+                // transforming the berries and eyeballs to new position.
+                transform.position = _newPos;
+            }
         }
     }
 }
